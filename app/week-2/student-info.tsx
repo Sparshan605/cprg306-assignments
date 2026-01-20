cf42c19a6e0c83f033f2680e4db119630146eeb3
Spiderman-cell <Sparshan144@gmail.com>
2026-01-20 14:25:27 -0700

    first assignemnt

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,12 @@
+import { link } from "fs";
+
+export default function StudentInfo() {
+  return (
+    <main>
+        <h1>Sparshan Koirala</h1>
+        <a href="https://github.com/Sparshan605/cprg306-assignments" target="_blank" rel="noopener noreferrer">
+    <button type="button">View on GitHub</button>
+        </a>
+    </main>
+  );
+}
