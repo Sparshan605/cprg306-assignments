2ede0b9d6f165c06edf12c706165eae7592ea516
Spiderman-cell <Sparshan144@gmail.com>
2026-01-27 13:55:53 -0700

    Week 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import Link from "next/link";
+import Week3page from "./week-3/page";
 
 export default function Home() {
   return (
     <>
       <main>
-        <h1>CPRG 306: Web Development 2 - Assignments</h1>
+        <h1 className="p-10 flex justify-center items-start font-semibold text-white-900 text-xl">CPRG 306: Web Development 2 - Assignments</h1>
+        <br></br>
+        <div className="flex justify-center items-star">
         <Link href="/week-2">
-          <button type="button">Week-2 Page</button>
+          <button className=" px-8 py-10 bg-red-600 text-white rounded-lg " type="button">Week-2 Page</button>
         </Link>
+        </div>
+        <br></br>
+        <div className="flex justify-center items-star">
+        <Link href="/week-3">
+        <button className="px-8 py-10 bg-green-600 text-white rounded-lg " type="button">Week-3 Page</button>
+        </Link>
+        </div>
       </main>
     </>
   );
