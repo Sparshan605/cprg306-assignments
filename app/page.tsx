0b4f142a236cea018d156dd1c93860aef69b5a90
Spiderman-cell <Sparshan144@gmail.com>
2026-02-03 14:28:56 -0700

    Week-4

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Week3page from "./week-3/page";
 
 export default function Home() {
   return (
@@ -7,17 +6,25 @@ export default function Home() {
       <main>
         <h1 className="p-10 flex justify-center items-start font-semibold text-white-900 text-xl">CPRG 306: Web Development 2 - Assignments</h1>
         <br></br>
-        <div className="flex justify-center items-star">
+        <div className="flex justify-center items-start">
         <Link href="/week-2">
           <button className=" px-8 py-10 bg-red-600 text-white rounded-lg " type="button">Week-2 Page</button>
         </Link>
         </div>
         <br></br>
-        <div className="flex justify-center items-star">
+        <div className="flex justify-center items-start">
         <Link href="/week-3">
         <button className="px-8 py-10 bg-green-600 text-white rounded-lg " type="button">Week-3 Page</button>
         </Link>
         </div>
+        <br />
+        <div className="flex justify-center items-start">
+        <Link href="week-4">
+        <button className="px-8 py-10 bg-blue-600 text-white rounded-lg " type="button">Week-4 Page</button>
+        </Link>
+        </div>
+        <br></br>
+        
       </main>
     </>
   );
---
+++ app/week-4/page.tsx
@@ -0,0 +1,8 @@
+import NewItemForm from "./new-item";
+export default function Week4Page() {
+  return (
+    <div className="mr-60 mt-40">
+      <NewItemForm />
+    </div>
+  );
+}
